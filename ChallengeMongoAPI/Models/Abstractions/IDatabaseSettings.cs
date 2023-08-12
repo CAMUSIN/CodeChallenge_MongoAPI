448e5694bd9ef814d166d513da729baeaac1a724
CAMUSIN <fergg8704@gmail.com>
2023-08-11 20:57:22 -0600

    Add: MongoDB Configuration

---
+++ ChallengeMongoAPI/Models/Abstractions/IDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace ChallengeMongoAPI.Models.Abstractions
+{
+    public interface IDatabaseSettings
+    {
+        string DatabaseName { get; set; }
+
+        string CollectionName { get; set; }
+
+        string ConnectionString { get; set; }
+    }
+}
--- ChallengeMongoAPI/Program.cs
+++ ChallengeMongoAPI/Program.cs
@@ -1,3 +1,8 @@
+using ChallengeMongoAPI.Models;
+using ChallengeMongoAPI.Models.Abstractions;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
 namespace ChallengeMongoAPI
 {
     public class Program
@@ -7,6 +12,14 @@ namespace ChallengeMongoAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.Configure<DatabaseSettings>(
+                builder.Configuration.GetSection("MongoDBSettings"));
+
+            builder.Services.AddSingleton<IDatabaseSettings>(
+            sp => sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
+
+            builder.Services.AddSingleton<IMongoClient>(s =>
+                new MongoClient(builder.Configuration.GetValue<string>("MongoDBSettings:ConnectionString")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
